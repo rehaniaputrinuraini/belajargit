f83a7221b66572bdf573321b96c40f01fafddf91
rehaniaputrinuraini <rehania2018putri@gmail.com>
2026-06-10 11:32:02 +0700

    commit pertama

---
+++ button.js
@@ -0,0 +1,55 @@
+//STRING (20)
+var judulFilm = "Tak Ingin Usai Disini";
+var namaHari = "Sabtu";
+var merekSepatu = "New Balance";
+var jenisTas = "Shoulder Bag";
+var warnaBaju = "Dusty Pink";
+var rasaEsKrim = "Matcha";
+var merkLaptop = "Lenovo";
+var namaPlanet = "Mars";
+var jenisTumbuhan = "Kaktus";
+var namaGunung = "Semeru";
+var jenisHewan = "Mamalia";
+var merkMobil = "Honda";
+var rasaRoti = "Pandan";
+var jenisPisau = "Pisau Daging";
+var merekTV = "Samsung";
+var namaNegara = "Indonesia";
+var jenisIkan = "Sapu Sapu";
+var namaBenua = "Asia";
+var merkJam = "Rolex";
+var jenisKain = "Katun";
+
+//NUMBER(15)
+var durasiTidur = 7;
+var jumlahPintu = 4;
+var ukuranSepatu = 42;
+var lebarMeja = 120;
+var panjangHalamanRumah = 500;
+var jumlahLampu = 6;
+var isiGalon = 10;
+var tebalBuku = 200;
+var kecepatanMotor = 80;
+var jumlahGarpu = 8;
+var beratTelur = 50;
+var tinggiKursi = 45;
+var luasTanah = 150;
+var diameterPiring = 25;
+var kedalamanKolam = 120;
+
+//BOOLEAN (15)
+var langitBiru = true;
+var airMengalirKeBawah = true;
+var apiPanas = true;
+var saljuDingin = true;
+var besiBerkarat = true;
+var kapasBerwarnaPink = false;
+var gulaRasanyaAsin = false;
+var esBatuPanas = false;
+var matahariTerbitDariTimur = true;
+var bulanBersinarPagi = false;
+var daunBerwarnaHijau = true;
+var duriMawarTajam = true;
+var kertasLebihBeratDariBesi = false;
+var anginTidakBerwarna = true;
+var tanahBerbauWangi = false;
